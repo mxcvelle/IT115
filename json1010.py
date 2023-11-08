03395120108f73b6b2815fb93516c9f4ea4897e2
Michelle Nguyen <missmichellenguyen01@gmail.com>
2023-11-08 10:19:50 -0800

    IT115

---
+++ json1010.py
@@ -0,0 +1,17 @@
+import json
+
+
+
+#Create the data dictionary
+
+data = {
+
+    'name': 'Michelle Nguyen',
+    'age':30,
+    'city':'New York,NY',
+    'interests': ['golf','running','videogames'],
+    'is_student': True
+
+}
+
+#I am making a change to prove that git status works pushing to the repo. 
