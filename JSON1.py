e59d4ecb7fe58ab9cbb922c2122b1d2a78042bf2
Michelle Nguyen <missmichellenguyen01@gmail.com>
2023-11-14 11:07:15 -0800

    updates

--- JSON1.py
+++ JSON1.py
@@ -7,6 +7,7 @@ import json
 
 data = {
 
+    
     'name': 'Michelle Nguyen',
     'age': 30,
     'city':'Mountlake Terrace, WA',
@@ -21,3 +22,22 @@ with open('data.json','w') as json_file:
     json.dump(data,json_file,indent=4)
 
 print('Data has been written to data.json')
+
+# 
+with open ('data.json','r') as json_file:
+
+    loaded_data = json.load(json_file)
+
+print("Successfully able to read data.json")
+print(loaded_data)
+
+#
+loaded_data['age'] = 29
+loaded_data['interests'].append('traveling')
+
+#
+with open('data.json','w') as json_file:
+
+    json.dump(loaded_data, json_file, indent =4)
+
+print('Modified data written to data.json')
